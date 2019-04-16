ecda2e252178b8d833672fd0fdec78837525dd81
Frietje <lukefromstarwars@gmail.com>
2019-04-16 06:03:15 +0200

    visibility

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,9 @@
 <template>
   <div class="home">
     <h1>Home page</h1>
-    <v-btn class="pink white--text">Click me</v-btn>
-    <v-btn depressed color="pink">Click again</v-btn>
-    <v-btn flat color="lime darken-4">Click flat</v-btn>
 
-    <v-btn depressed class="pink white--text">
-      <v-icon right>donut_small</v-icon>Click again
-    </v-btn>
-    <v-btn depressed class="red white--text">
-      <v-icon left>done_all</v-icon>Click again
-    </v-btn>
-
-    <v-btn depressed color="red">
-      <v-icon>event</v-icon>
-      <span>&nbsp;Click again</span>
-    </v-btn>
-
-    <v-btn depressed small color="red">
-      <v-icon>event</v-icon>
-      <span>&nbsp;Click again</span>
-    </v-btn>
-
-    <v-btn fab flat small dark color="success">
-      <v-icon small>event</v-icon>
-    </v-btn>
+    <v-btn depressed color="lime darken-4" class="hidden-md-and-down">Click flat</v-btn>
+    <v-btn depressed color="lime" class="hidden-md-and-up">Click flat</v-btn>
   </div>
 </template>
 
