e9796006759eada9f46082953fb54a92b7912b2b
Frietje <lukefromstarwars@gmail.com>
2019-04-16 05:55:59 +0200

    buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="home">
     <h1>Home page</h1>
-    <p
-      class="red white--text"
-    >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nihil eos labore vero unde modi, laborum reprehenderit inventore reiciendis asperiores corrupti veniam? Illum officia quidem nesciunt rerum, non corporis dolor!</p>
-    <p
-      class="orange lighten-1 orange-text accent-4"
-    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis sapiente, consectetur cumque hic assumenda quos explicabo non quis, veniam, earum officiis natus maxime corrupti optio vitae consequuntur provident doloribus iure!</p>
-    <h1 class="display-4">Massive display</h1>
-    <h4 class="diplay-1">Smaller display</h4>
-    <p class="headline">this is a headline</p>
-    <p class="subheading text-lowercase">This is a subheading</p>
-    <p class="caption text-uppercase">This is a caption</p>
+    <v-btn class="pink white--text">Click me</v-btn>
+    <v-btn depressed color="pink">Click again</v-btn>
+    <v-btn flat color="lime darken-4">Click flat</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon right>donut_small</v-icon>Click again
+    </v-btn>
+    <v-btn depressed class="red white--text">
+      <v-icon left>done_all</v-icon>Click again
+    </v-btn>
+
+    <v-btn depressed color="red">
+      <v-icon>event</v-icon>
+      <span>&nbsp;Click again</span>
+    </v-btn>
+
+    <v-btn depressed small color="red">
+      <v-icon>event</v-icon>
+      <span>&nbsp;Click again</span>
+    </v-btn>
+
+    <v-btn fab flat small dark color="success">
+      <v-icon small>event</v-icon>
+    </v-btn>
   </div>
 </template>
 
