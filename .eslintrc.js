e879b0ebc6ac9194f60c17fa6774b9ec9bf1e67b
lukefromstarwars <lukefromstarwars@gmail.com>
2019-04-17 13:49:43 +0200

    form date picker

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,6 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-implicit-coercion": {
-      "boolean": false
-    }
   },
   parserOptions: {
     parser: "babel-eslint"
--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -18,6 +18,21 @@
             v-model="content"
             prepend-icon="edit"
           ></v-textarea>
+          <v-menu :close-on-content-click="false" full-width max-width="290">
+            <v-text-field
+              prepend-icon="calendar_today"
+              slot="activator"
+              :value="formattedDate"
+              clearable
+              label="Due date"
+              readonly
+            ></v-text-field>
+            <v-date-picker
+              v-model="due"
+              @change="menu2 = false"
+            ></v-date-picker>
+          </v-menu>
+          <v-spacer></v-spacer>
           <v-btn flat class="success mx-0 mt-3" @click="submit"
             >Add project</v-btn
           >
@@ -27,16 +42,23 @@
   </v-dialog>
 </template>
 <script>
+import format from "date-fns/format";
 export default {
   data() {
     return {
       title: "",
-      content: ""
+      content: "",
+      due: null
     };
   },
   methods: {
     submit() {
-      console.log(this.title, this.content);
+      console.log(this.title, this.content, this.due);
+    }
+  },
+  computed: {
+    formattedDate() {
+      return this.due ? format(this.due, "Do MMM YYYY") : null;
     }
   }
 };
