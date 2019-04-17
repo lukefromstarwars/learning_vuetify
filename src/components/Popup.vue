2312e9afaf56289ed980b5aab8771dcbeb220df7
lukefromstarwars <lukefromstarwars@gmail.com>
2019-04-17 14:07:19 +0200

    form validation

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -6,31 +6,33 @@
     <v-card
       ><v-card-title primary-title><h2>Add a new project</h2> </v-card-title
       ><v-card-text
-        ><v-form class="px-3"
+        ><v-form class="px-3" ref="form"
           ><v-text-field
             label="Title"
             v-model="title"
             prepend-icon="folder"
+            :rules="inputRules"
           ></v-text-field>
+          <v-spacer></v-spacer>
           <v-textarea
             solo
             label="Information"
             v-model="content"
             prepend-icon="edit"
+            :rules="inputRules"
           ></v-textarea>
-          <v-menu :close-on-content-click="false" full-width max-width="290">
+          <v-spacer></v-spacer>
+          <v-menu :close-on-content-click="false" full-width v-model="menu">
             <v-text-field
-              prepend-icon="calendar_today"
+              prepend-icon="date_range"
               slot="activator"
               :value="formattedDate"
               clearable
               label="Due date"
               readonly
+              :rules="inputRules"
             ></v-text-field>
-            <v-date-picker
-              v-model="due"
-              @change="menu2 = false"
-            ></v-date-picker>
+            <v-date-picker v-model="due" @change="menu = false"></v-date-picker>
           </v-menu>
           <v-spacer></v-spacer>
           <v-btn flat class="success mx-0 mt-3" @click="submit"
@@ -48,17 +50,24 @@ export default {
     return {
       title: "",
       content: "",
-      due: null
+      due: null,
+      menu: false,
+      inputRules: [
+        v => !!v || "This field is required",
+        v => v.length > 3 || "Minimum length is 3 characters"
+      ]
     };
   },
   methods: {
     submit() {
-      console.log(this.title, this.content, this.due);
+      if (this.$refs.form.validate()) {
+        console.log(this.title, this.content, this.due);
+      }
     }
   },
   computed: {
     formattedDate() {
-      return this.due ? format(this.due, "Do MMM YYYY") : null;
+      return this.due ? format(this.due, "Do MMM YYYY") : "";
     }
   }
 };
