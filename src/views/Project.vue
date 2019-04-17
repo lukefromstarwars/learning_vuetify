0d499e105090a41f8bc3de3d597aab4f72d59d30
lukefromstarwars <lukefromstarwars@gmail.com>
2019-04-17 10:53:23 +0200

    expansion panel

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,9 +1,88 @@
 <template>
   <div>
     <h1 class="subheading grey--text">Project</h1>
-    <v-container class="my-3"> content </v-container>
+    <v-container class="my-3">
+      <!--       <v-expansion-panel>
+        <v-expansion-panel-content v-for="(item, i) in 5" :key="i">
+          <template v-slot:header>
+            <div>Item</div>
+          </template>
+          <v-card>
+            <v-card-text
+              >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.</v-card-text
+            >
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+ -->
+      <v-expansion-panel>
+        <v-expansion-panel-content
+          v-for="project in myProjects"
+          :key="project.title"
+        >
+          <template v-slot:header>
+            <div>{{ project.title }}</div>
+          </template>
+          <v-card>
+            <v-card-text class="px-4 grey--text"
+              ><div class="font-weight-bold">{{ project.due }}</div>
+              {{ project.content }}</v-card-text
+            >
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-container>
   </div>
 </template>
 <script>
-export default {};
+import _ from "lodash";
+
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          title: "Design a new website",
+          person: "The Net Ninja",
+          due: "1st Jan 2019",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Code up the homepage",
+          person: "Chun Li",
+          due: "10th Jan 2019",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Design video thumbnails",
+          person: "Ryu",
+          due: "20th Dec 2018",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Create a community forum",
+          person: "Gouken",
+          due: "20th Oct 2018",
+          status: "overdue",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        }
+      ]
+    };
+  },
+  computed: {
+    myProjects() {
+      return _.filter(this.projects, { person: "The Net Ninja" });
+    }
+  }
+};
 </script>
